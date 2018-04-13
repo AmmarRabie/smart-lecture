e8bb0efe171ec2c4f53d39af7efe38f6b213260c
AmmarRabie <ammaralsayed55@gmail.com>
2018-04-14 01:03:52 +0200

    [auth]: Set the result of successing back to the acivity instead of preventing closing it with back button. [grouplist]: Now Fragment and Presenter only instintiated if the result of login is ok, else the app is closed. This way resolved the error of freezing the presenter of grouplist as it started before user is logged in.

--- app/src/main/java/cmp/sem/team8/smarlecture/grouplist/GroupListActivity.java
+++ app/src/main/java/cmp/sem/team8/smarlecture/grouplist/GroupListActivity.java
@@ -1,33 +1,23 @@
 package cmp.sem.team8.smarlecture.grouplist;
 
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
 import android.widget.Toast;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import cmp.sem.team8.smarlecture.R;
 import cmp.sem.team8.smarlecture.auth.LoginActivity;
 import cmp.sem.team8.smarlecture.common.util.ActivityUtils;
-import cmp.sem.team8.smarlecture.joinsession.JoinedSession;
 import cmp.sem.team8.smarlecture.profile.ProfileActivity;
+import es.dmoral.toasty.Toasty;
 
 public class GroupListActivity extends AppCompatActivity {
 
 
+    private static final int INTENT_REQUEST_LOGIN = 1;
     private GroupListPresenter mGroupListPresenter;
 
 
@@ -37,18 +27,7 @@ public class GroupListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_grouplist);
         setTitle("Groups");
         Intent i = new Intent(this, LoginActivity.class);
-        startActivity(i);
-
-        GroupListFragment grouplistfragment = (GroupListFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.contentFrame);
-
-        if (grouplistfragment == null) {
-            grouplistfragment = GroupListFragment.newInstance();
-            ActivityUtils.addFragmentToActivity(getSupportFragmentManager(),
-                    grouplistfragment, R.id.contentFrame);
-        }
-
-        mGroupListPresenter = new GroupListPresenter(grouplistfragment);
+        startActivityForResult(i, INTENT_REQUEST_LOGIN);
     }
 
     @Override
@@ -68,5 +47,29 @@ public class GroupListActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case INTENT_REQUEST_LOGIN:
+                if (resultCode != RESULT_OK) {
+                    // login cancelled, end the application
+                    finish();
+                    return;
+                }
+                GroupListFragment grouplistfragment = (GroupListFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.contentFrame);
+
+                if (grouplistfragment == null) {
+                    grouplistfragment = GroupListFragment.newInstance();
+                    ActivityUtils.addFragmentToActivity(getSupportFragmentManager(),
+                            grouplistfragment, R.id.contentFrame);
+                }
+                mGroupListPresenter = new GroupListPresenter(grouplistfragment);
+                return;
+        }
     }
 }
