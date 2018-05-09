ef4e91849859057b6988d09736cd62960d876650
AmmarRabie <ammaralsayed55@gmail.com>
2018-05-09 20:59:07 +0200

    doc the internetConnectivityReceiver.

--- app/src/main/java/cmp/sem/team8/smarlecture/common/InternetConnectivityReceiver.java
+++ app/src/main/java/cmp/sem/team8/smarlecture/common/InternetConnectivityReceiver.java
@@ -11,11 +11,14 @@ import android.net.NetworkInfo;
  * Created by AmmarRabie on 06/04/2018.
  */
 
-
+/**
+ * Helper to notify clients with internet connectivity states (online - offline)
+ * Adapt the internet change receiver to be easy used in the app
+ */
 public class InternetConnectivityReceiver {
 
-    OnInternetConnectionChangeListener mOnInternetConnectionChangeListener;
-    BroadcastReceiver connectivityReceiver = new BroadcastReceiver() {
+    private OnInternetConnectionChangeListener mOnInternetConnectionChangeListener;
+    private BroadcastReceiver connectivityReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             if (intent.getAction().equals("android.net.conn.CONNECTIVITY_CHANGE")) {
@@ -37,18 +40,37 @@ public class InternetConnectivityReceiver {
         mOnInternetConnectionChangeListener = onInternetConnectionChangeListener;
     }
 
+    /**
+     * start monitor the connection
+     *
+     * @param context context of the app
+     * @return the class itself as this function can be used like that:
+     * mConnectivityReceiver = new InternetConnectivityReceiver(context).start();
+     */
     public InternetConnectivityReceiver start(Context context) {
         IntentFilter intentFilter = new IntentFilter("android.net.conn.CONNECTIVITY_CHANGE");
         context.registerReceiver(connectivityReceiver, intentFilter);
         return this;
     }
 
+    /**
+     * End the listening, this should be called so that free the memory
+     *
+     * @param context context that started the receiving in {@link #start(Context)} method
+     */
     public void end(Context context) {
         context.unregisterReceiver(connectivityReceiver);
     }
 
     public interface OnInternetConnectionChangeListener {
+        /**
+         * called when connection lost (offline)
+         */
         void onInternetConnectionLost();
+
+        /**
+         * called when connection back (online)
+         */
         void onInternetConnectionBack();
     }
 }
