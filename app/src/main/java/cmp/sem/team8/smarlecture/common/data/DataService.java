a0c19326cbbb4a651f7bcdc260338cd540867100
AmmarRabie <ammaralsayed55@gmail.com>
2018-05-09 19:33:36 +0200

    doc and edit DataService methods.

--- app/src/main/java/cmp/sem/team8/smarlecture/common/data/DataService.java
+++ app/src/main/java/cmp/sem/team8/smarlecture/common/data/DataService.java
@@ -33,42 +33,49 @@ import cmp.sem.team8.smarlecture.common.data.model.UserModel;
  * {@link Listen})
  */
 public interface DataService {
-
+    /**
+     * Get the user by its id
+     *
+     * @param userId   the id of the user caller want to get
+     * @param callback Send the data to the caller or tell him with errors
+     */
     void getUser(String userId, Get<UserModel> callback);
 
+    /**
+     * @param Grade
+     * @param userId
+     * @param callback
+     */
     void getUserGrade(int Grade, String userId, Get<UserGradeModel> callback);
 
+    /**
+     * Insert new user into the data source. Note that this method doesn't authenticate the user
+     * Caller should handle authentication outside this method
+     *
+     * @param userModel user to insert him in the data source
+     * @param callback  Send the feedback to the caller or tell him with errors
+     */
     void insertUser(UserModel userModel, Insert<Void> callback);
 
     void updateGroupGrades(String groupId, ArrayList<String> ids, ArrayList<Integer> grade, final Update callback);
 
+    /**
+     * Change the user name of the user already exists in the system
+     *
+     * @param userId   the id of the user
+     * @param newName  new name to update it in the data source
+     * @param callback notify success or tell him with errors
+     */
     void updateUserName(String userId, String newName, Update callback);
 
     /**
-     * return all sessions that passed user is a member in their groups
+     * Change the profile image of the user already exists in the system
      *
-     * @param userId   the user id you want to get sessions he is a member in
-     * @param callback return the data on this callback
+     * @param userId        the id of the user
+     * @param newImageBytes new image in bytes to update it in the data source
+     * @param callback      notify success or tell him with errors
      */
-//    void getSessionsForUser(String userId, Get<ArrayList<SessionForUserModel>> callback); // for student
-
-
     void updateUserProfileImage(String userId, byte[] newImageBytes, Update callback);
-
-
-    /**
-     * return all sessions that passed user is a member in their groups filtered by flags
-     *
-     * @param userId        the user id you want to get sessions he is a member in
-     * @param callback      return the data on this callback
-     * @param withClosed    if false, the sessions returned will exclude closed ones
-     * @param withOpened    if false, the sessions returned will exclude opened ones
-     * @param withNotActive if false, the sessions returned will exclude not active ones
-     */
-/*    void getSessionsForUser(String userId, Get<ArrayList<SessionForUserModel>> callback
-            , boolean withClosed
-            , boolean withOpened
-            , boolean withNotActive);*/
 
     /**
      * return all sessions that passed user is a member in their groups filtered by flags <B><I>one by one</B>
@@ -100,99 +107,354 @@ public interface DataService {
      */
     void getGroupInvitationsForUser(String userId, Get<GroupInvitationModel> callback);
 
-    void getSessionsOfGroup(String groupId, Get<ArrayList<SessionModel>> callback); // for lecturer
-
-    void getGroupAndItsSessionNameList(String groupId, Get<GroupStatisticsModel> callback); // for lecturer
+    void getGroupAndItsSessionNameList(String groupId, Get<GroupStatisticsModel> callback);
 
     void getGroupGrade(String groupId, Get<ArrayList<UserGradeModel>> callback);
 
-    void inviteUserToGroup(String email, String groupId, Insert<UserModel> callback); // add a new student
+    /**
+     * Invite a new user to the passed group by user email. Note that this function only invite
+     * the user and don't make him a member.
+     *
+     * @param email    the email of invited user
+     * @param groupId  identify the group caller want to add this user to
+     * @param callback feedback return the user that is just invited or tell the error
+     * @see #acceptFollowingGroup(String, String, Update) {@link #refuseFollowingGroup(String, String, Update)}
+     */
+    void inviteUserToGroup(String email, String groupId, Insert<UserModel> callback);
 
+    /**
+     * Accept the user into the passed group. after this method success the passed user becomes a
+     * group member of the passed group. this function should put the new user into not activated
+     * sessions of the group
+     *
+     * @param userId   user that accept the invitation
+     * @param groupId  group that has been accepted
+     * @param callback notify success or tell him with errors
+     */
     void acceptFollowingGroup(String userId, String groupId, Update callback);
 
+    /**
+     * delete the user from the passed group. after this method success the passed user becomes no
+     * more in this group (not invited - not a member).
+     *
+     * @param userId   user that refuse the invitation
+     * @param groupId  group that has been refused
+     * @param callback notify success or tell him with errors
+     */
     void refuseFollowingGroup(String userId, String groupId, Update callback);
 
-    //get session name and group owner for the session id to be viewed for the student when he joins a session
+    /**
+     * get session name and group owner for the session id to be viewed for the student when he joins a session
+     *
+     * @param sessionID the session to get its data
+     * @param groupID   group id this session is related to
+     * @param callback  return the data on this callback or tell the error
+     */
     void getJoinedSessionInfo(String sessionID, String groupID, Get<SessionForUserModel> callback);
 
 
-    //check for session status
+    /**
+     * listen the changes of the session status on tha passed session
+     *
+     * @param sessionID the id of the session that will be listen to
+     * @param callback  return that status every time it changes till it be forgotten
+     * @return the callback passed to the function to easy {@link #forget(Listen)} it in the
+     * proper time
+     */
     Listen listenSessionStatus(String sessionID, Listen<SessionStatus> callback);
 
 
-    //gets the objectives for a session
+    /**
+     * get the objectives of a session
+     *
+     * @param sessionID session id to get its objectives
+     * @param callback  return the data on this callback
+     */
     void getObjectives(String sessionID, Get<ArrayList<ObjectiveModel>> callback);
 
-    //update new average rating of objective
+    //
+
+    /**
+     * update new average rating of objective
+     *
+     * @param sessionID           session id this objective related to
+     * @param objectiveID         identify the id of this objective
+     * @param newObjectiveRating  new objective rating to update it in the data source
+     * @param newNumberUsersRated new count of the users rating this objective
+     * @param callback            notify success or tell him with errors
+     */
     void updateObjectivesRating(String sessionID, String objectiveID, Float newObjectiveRating, Integer newNumberUsersRated, Update callback);
 
+    /**
+     * Insert new objective in passed session
+     *
+     * @param sessionID      identify the session to insert this objective in
+     * @param addedObjective description of the objective
+     * @param isOffline      whether or not the device in in offline mode
+     * @param callback       return the inserted objective or tell the error
+     */
     void insertObjective(String sessionID, String addedObjective, boolean isOffline, Insert<ObjectiveModel> callback);
 
+    /**
+     * get the group members and invited members in the passed group
+     *
+     * @param groupId  identify the group to get the members of it
+     * @param callback return the data on this callback or tell the error
+     */
     void getGroupMembers(String groupId, Get<ArrayList<InvitedUserModel>> callback);
 
+    /**
+     * get session status of passed session
+     *
+     * @param sessionId identify the session
+     * @param callback  return the data on this callback or tell the error
+     * @see #listenSessionStatus(String, Listen)
+     */
     void getSessionStatus(String sessionId, Get<SessionStatus> callback);
 
+    /**
+     * change or insert the passed status to passed session
+     *
+     * @param sessionId identify the session
+     * @param status    new status to be put to the session status
+     * @param callback  notify success or tell errors
+     */
     void setSessionStatus(String sessionId, SessionStatus status, Insert<Void> callback);
 
+    /**
+     * change or insert the passed status to attendance status in passed session
+     *
+     * @param sessionId identify the session
+     * @param status    new status to be put to the attendance status of the session
+     * @param callback  notify success or tell errors
+     */
     void setAttendanceStatus(String sessionId, AttendanceStatus status, Update callback);
 
+    /**
+     * change or insert the passed secret to passed session
+     *
+     * @param sessionId identify the session
+     * @param secret    new secret to update the session with
+     * @param callback  notify success or tell errors
+     */
     void setSessionSecret(String sessionId, String secret, Update callback);
 
+    /**
+     * listen the changes of the members of passed session and return the data <B>One by one</B>
+     *
+     * @param sessionId the id of the session that will be listen to its members
+     * @param callback  return any changed member every time it changes till it be forgotten
+     * @return the callback passed to the function to easy {@link #forget(Listen)} it in the
+     * proper time
+     */
     Listen ListenSessionMembers(String sessionId, Listen<MemberModel> callback);
 
+    /**
+     * update the attendance of a session member
+     *
+     * @param sessionId identify the session
+     * @param memberId  identify the user in this session to update his attendance
+     * @param isAttend  if true, the passed member will be attend in the passed session
+     * @param callback  notify success or tell errors
+     */
     void setMemberAttendance(String sessionId, String memberId, boolean isAttend, Update callback);
 
+    /**
+     * Add a new note to a member in a given session
+     *
+     * @param sessionId identify the session that this member is in
+     * @param memberId  identify the member this note will go to him
+     * @param noteText  the note text
+     * @param callback  return the inserted note back to the caller or tell him with errors
+     */
     void addNote(String sessionId, String memberId, String noteText, Insert<NoteModel> callback);
 
+    /**
+     * delete a note of session member
+     *
+     * @param sessionId identify the session this member is in
+     * @param memberId  identify the member that the note will be removed from
+     * @param noteId    identify the note to be removed from member token notes
+     * @param callback  whether or not the deletion is completed successfully
+     */
     void deleteNote(String sessionId, String memberId, String noteId, Delete callback);
 
+    /**
+     * Update objective description
+     *
+     * @param objectiveID          identify the objective to be updated
+     * @param sessionID            identify the session
+     * @param objectiveDescription new description of the objective
+     * @param isOffline            whether or not the device is in offline mode
+     * @param callback             notify success or tell him with errors
+     */
     void editObjective(String objectiveID, String sessionID, String objectiveDescription, boolean isOffline, Update callback);
 
+    /**
+     * delete a note of session member
+     *
+     * @param sesisonID   identify the session this objective is related to
+     * @param objectiveID identify the objective to be removed from session objectives
+     * @param callback    whether or not the deletion is completed successfully
+     */
     void deleteObjective(String objectiveID, String sesisonID, boolean isOffline, Delete callback);
 
+    /**
+     * get all sessions of the passed group in session model representation
+     *
+     * @param groupId  identify the group to get its sessions
+     * @param callback return the sessions of the group or tell the errors
+     */
     void getGroupSessions(String groupId, Get<ArrayList<SessionModel>> callback);
 
+    /**
+     * insert new session into passed group
+     *
+     * @param groupId     identify the group to insert this session to it
+     * @param sessionId   the new session id, this is passed to the function as it is randomly generated
+     *                    depending on system business logic
+     * @param sessionName the name of the session
+     * @param callback    whether or not the insert is completed successfully
+     */
     void addSession(String groupId, String sessionId, String sessionName, Insert<Void> callback);
 
+    /**
+     * Edit the name of the session
+     *
+     * @param sessionId   identify the session
+     * @param sessionName the new name of the session
+     * @param isOffline   whether or not the device is in offline mode
+     * @param callback    notify success or tell him with errors
+     */
     void editSession(String sessionId, String sessionName, boolean isOffline, Update callback);
 
+    /**
+     * delete a session by its id
+     *
+     * @param sessoinId identify the session
+     * @param isOffline whether or not the device is in offline mode
+     * @param callback  whether or not the deletion is completed successfully
+     */
     void deleteSession(String sessoinId, boolean isOffline, Delete callback);
 
+    /**
+     * delete a group by its id
+     *
+     * @param groupId   identify the group to be deleted
+     * @param isOffline whether or not the device is in offline mode
+     * @param callback  whether or not the deletion is completed successfully
+     */
     void deleteGroupById(String groupId, boolean isOffline, Delete callback);
 
+    /**
+     * insert new group in the data source
+     *
+     * @param userId    the id of the owner
+     * @param groupName the group name
+     * @param isOffline whether or not the device is in offline mode
+     * @param callback  return the id of the inserted group or tell the errors
+     */
     void addGroup(String userId, String groupName, boolean isOffline, Insert<String> callback);
 
+    /**
+     * Change the name of the group
+     *
+     * @param groupId   identify the group to change its name
+     * @param groupName new name of the group
+     * @param isOffline whether or not the device is in offline mode
+     * @param callback  notify success or tell him with errors
+     */
     void updateGroup(String groupId, String groupName, boolean isOffline, Update callback);
 
+    /**
+     * Get the group id given a session id in it
+     *
+     * @param sessionId the session id caller want to find its group id
+     * @param callback  return the group id or tell the errors
+     */
     void getGroupId(String sessionId, Get<String> callback);
 
+    /**
+     * get the group of the passed user (All groups this user is the owner of them)
+     *
+     * @param userId   the user id to get all its created groups
+     * @param callback return the list of the groups or tell the errors
+     */
     void getGroupsForUser(String userId, Get<ArrayList<GroupModel>> callback);
 
+    /**
+     * get a representation of group data to export it in any formats
+     *
+     * @param groupId  identify the group to get its data
+     * @param callback return the group data or tell the errors
+     */
     void getGroupInfoForExport(String groupId, Get<FileModel> callback);
 
+    /**
+     * get the session data by the session id
+     *
+     * @param sessionId identify the session to be retrieved
+     * @param callback  return the data of the session or tell errors
+     */
     void getSessionById(String sessionId, Get<SessionModel> callback);
 
+    /**
+     * listen the changes of the attendance status of tha passed session
+     *
+     * @param sessionId the id of the session that will be listen to its attendance status
+     * @param callback  return that status every time it changes till it be forgotten
+     * @return the callback passed to the function to easy {@link #forget(Listen)} it in the
+     * proper time
+     */
     Listen listenAttendanceStatus(String sessionId, Listen<AttendanceStatus> callback);
 
+    /**
+     * get a group messages
+     *
+     * @param groupId  identify the group to get its messages
+     * @param callback return the messages of this group or tell the errors
+     */
     void getGroupMessages(String groupId, Get<ArrayList<GroupMessageModel>> callback);
 
-    // gets Question for a session
-    Listen ListenSessionQuestions(String sessionID, Listen<QuestionModel> callback);
+    /**
+     * listen the changes of the questions of passed session and return the data <B>One by one</B>
+     *
+     * @param sessionId the id of the session that will be listen to its questions
+     * @param callback  return any changed question every time it changes till it be forgotten
+     * @return the callback passed to the function to easy {@link #forget(Listen)} it in the
+     * proper time
+     */
+    Listen ListenSessionQuestions(String sessionId, Listen<QuestionModel> callback);
 
+    /**
+     * Insert new question into session
+     *
+     * @param sessionId identify the session that question will be inserted to
+     * @param askerId   the owner of the question (asker)
+     * @param text      the question text
+     * @param callback  return the question that has just inserted or tell the errors
+     */
     void addQuestionToSession(String sessionId, String askerId, String text, Insert<QuestionModel> callback);
 
-    void deleteGroupMember(String groupId, String memberId, Delete callback);
-
-
+    /**
+     * send group message to all group members of the passed group
+     *
+     * @param groupId  identify the group the message will be sent to its members
+     * @param message  the message text that will be sent
+     * @param callback whether the insertion is success or not
+     */
     void sendGroupNotification(String groupId, String message, Insert<Void> callback);
 
     /**
-     * @param listener
+     * Remove notifying callbacks with changing. After this function is called by a given listener
+     * this listener will not receive {@link Listen#onDataReceived}yhhhhh even data changed
+     *
+     * @param listener the listener passed to any listen function from {@link DataService}
      */
     void forget(Listen listener);
 
     /**
-     * The attendance status between the data source and clients of it
+     * The attendance status between the data service and clients of it
      */
     enum AttendanceStatus {
         NOT_ACTIVATED("not-activated"), OPEN("open"), CLOSED("closed");
